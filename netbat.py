30f1ca07fd502168a8caf51c9355a6d3bdd2d7d3
andres-root <info@andresroot.co>
2018-09-16 11:56:01 -0500

    WIP

--- netbat.py
+++ netbat.py
@@ -1,1 +1,38 @@
-netbat.py
+import sys
+import socket
+import getopt
+import threading
+import subprocess
+
+
+LISTEN = False
+PORT = 0
+EXECUTE = ''
+COMMAND = False
+UPLOAD_DESTINATION = ''
+TARGET = ''
+UPLOAD = False
+
+
+# TODO: Implement this function
+def usage():
+    print('Need a target.')
+    pass
+
+
+def main():
+    global LISTEN
+    global PORT
+    global EXECUTE
+    global COMMAND
+    global UPLOAD_DESTINATION
+    global TARGET
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hle:t:p:cu', ['help', 'listen', 'execute', 'target', 'port', 'upload'])
+    except Exception as err:
+        print(str(err))
+        usage()
